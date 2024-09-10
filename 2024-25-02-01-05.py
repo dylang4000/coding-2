455b44548406f33c2111c498ba53fc892d4b689f
dylang4000 <davis948@student.pcsstn.net>
2024-09-10 11:26:04 -0500

    Update 2024-25-02-01-05.py

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -4,21 +4,33 @@ from turtle import *
 import tkinter
 s=Screen()
 s.setup(1020,1000)
+boxW=0
+boxH=0
 size=numinput('Enter an odd number',"Enter an odd number")
+
+
+boxW=round(1020/size)
+boxH=round(1000/size)
+
+
+    
+    
+
+
 def square():
     global size
     for i in range(int(size)):
         pendown()
-        forward(200)
+        forward(boxW)
         right(90)
-        forward(200)
+        forward(boxH)
         right(90)
-        forward(200)
+        forward(boxW)
         right(90)
-        forward(200)
+        forward(boxH)
         penup()
         right(90)
-        forward(200)
+        forward(boxW)
 goto(-507,500)    
   
 print(window_height())
