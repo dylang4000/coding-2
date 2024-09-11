65190fa941c505cd5e83c6c80314f5dbc11cc089
Dylan <davis948@student.pcsstn.net>
2024-09-11 09:45:58 -0500

    commit

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -1,29 +1,42 @@
-#ask user for odd number, create grid using odd number
-
 from turtle import *
 import tkinter
 s=Screen()
 s.setup(1020,1000)
+boxW=0
+boxH=0
+boxCords=[]
 size=numinput('Enter an odd number',"Enter an odd number")
-def square():
-    global size
-    for i in range(int(size)):
-        pendown()
-        forward(200)
-        right(90)
-        forward(200)
-        right(90)
-        forward(200)
-        right(90)
-        forward(200)
-        penup()
-        right(90)
-        forward(200)
-goto(-507,500)    
-  
+
+boxW=round(1020/size)
+boxH=round(1020/size)
+
+penup()
+
+def square(boxW):
+    print('fart')
+
+
+
+
+
+
+    
+#    for i in range(int(size)):
+#        pendown()
+#        forward(boxW)
+#        right(90)
+#        forward(boxH)
+#        right(90)
+#        forward(boxW)
+#        right(90)
+#        forward(boxH)
+#        penup()
+#        right(90)
+#        forward(boxW)
+goto(-507,507)    
+print()
 print(window_height())
-for i in range(int(size)):
-    square()
+square(size)
 
 
 mainloop()
