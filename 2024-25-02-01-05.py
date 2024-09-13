403aeca70be3f5c7e926a8043c43d01c90edef1c
dylang4000 <davis948@student.pcsstn.net>
2024-09-13 13:18:00 -0500

    Update 2024-25-02-01-05.py

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -1,25 +1,28 @@
-import tkinter.colorchooser
+import tkinter
 from turtle import *
 import tkinter
 s=Screen()
-s.setup(1020,1000)
-tkinter.colorchooser.askcolor()
 size=numinput('Enter an odd number',"Enter an odd number")
-while size % 2 ==0:
-    size=numinput('Enter an ODD number',"Enter an ODD number")
+s.setup(1020,1000)
 boxW=round(1020/size)
 boxH=round(1000/size)
 positionT=[]
 rowcount=0
+
+while size % 2 ==0:
+    size=numinput('Enter an ODD number',"Enter an ODD number")
+
 def nextrow():
     goto(-507,500)
     goto(-507,ycor()-boxH*rowcount)
 
-speed(0) 
+
 def square():
     global size
     global rowcount
+    global positionT
     for i in range(int(size)):
+        positionT.append([xcor(),ycor()])
         pendown()
         forward(boxW)
         right(90)
@@ -32,11 +35,14 @@ def square():
         right(90)
         forward(boxW)
     rowcount+=1
-    
-penup()
-goto(-507,500)    
-for i in range(int(size)):
-    square()
-    nextrow()
-
+def grid():
+    penup()
+    goto(-507,500)    
+    for i in range(int(size)):
+        square()
+        nextrow()
+speed(0) 
+grid()
+onkeypress(print("hello"),"a")
+listen()
 mainloop()
