4bed268167dc1faeb77d31a3daec9a2b5b511f2f
Dylan <davis948@student.pcsstn.net>
2024-09-13 13:18:26 -0500

    commit

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -1,26 +1,48 @@
+import tkinter
 from turtle import *
 import tkinter
 s=Screen()
-s.setup(1020,1000)
 size=numinput('Enter an odd number',"Enter an odd number")
+s.setup(1020,1000)
+boxW=round(1020/size)
+boxH=round(1000/size)
+positionT=[]
+rowcount=0
+
+while size % 2 ==0:
+    size=numinput('Enter an ODD number',"Enter an ODD number")
+
+def nextrow():
+    goto(-507,500)
+    goto(-507,ycor()-boxH*rowcount)
+
+
 def square():
     global size
+    global rowcount
+    global positionT
     for i in range(int(size)):
+        positionT.append([xcor(),ycor()])
         pendown()
-        forward(200)
+        forward(boxW)
         right(90)
-        forward(200)
+        forward(boxH)
         right(90)
-        forward(200)
+        forward(boxW)
         right(90)
-        forward(200)
+        forward(boxH)
         penup()
         right(90)
-        forward(200)
-goto(-507,500)    
-  
-print(window_height())
-square()
-
-
+        forward(boxW)
+    rowcount+=1
+def grid():
+    penup()
+    goto(-507,500)    
+    for i in range(int(size)):
+        square()
+        nextrow()
+speed(0) 
+grid()
+onkeypress(print("hello"),"a")
+listen()
 mainloop()
