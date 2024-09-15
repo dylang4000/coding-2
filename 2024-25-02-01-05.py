c66f617d1fa2c2190e35d10ef2bd9d6e8adcef6c
dylang4000 <davis948@student.pcsstn.net>
2024-09-14 18:00:26 -0800

    Update 2024-25-02-01-05.py

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -1,48 +1,62 @@
-import tkinter
 from turtle import *
-import tkinter
+
+width(5)
 s=Screen()
-size=numinput('Enter an odd number',"Enter an odd number")
 s.setup(1020,1000)
+size=int(numinput('Enter an odd number',"Enter an odd number"))
+hideturtle()
 boxW=round(1020/size)
 boxH=round(1000/size)
 positionT=[]
 rowcount=0
+boxcount=0
+rowsCount=0
+columnCount=0
+
 
 while size % 2 ==0:
     size=numinput('Enter an ODD number',"Enter an ODD number")
 
-def nextrow():
-    goto(-507,500)
-    goto(-507,ycor()-boxH*rowcount)
 
-
-def square():
-    global size
-    global rowcount
-    global positionT
-    for i in range(int(size)):
-        positionT.append([xcor(),ycor()])
-        pendown()
-        forward(boxW)
-        right(90)
-        forward(boxH)
-        right(90)
-        forward(boxW)
-        right(90)
-        forward(boxH)
-        penup()
-        right(90)
-        forward(boxW)
-    rowcount+=1
 def grid():
+    global columnCount
+    global rowsCount
     penup()
-    goto(-507,500)    
+    goto(-510,500)
+    pendown()
+    rowsCount+=1
+    forward(1020)
     for i in range(int(size)):
-        square()
-        nextrow()
+        
+        penup()
+        goto(-510,500-(boxW*rowsCount))
+        pendown()
+        forward(1020)
+        rowsCount+=1
+    
+    right(90)
+    penup()
+    goto(-510,500)
+    pendown()
+    columnCount+=1
+    forward(1020)
+    for i in range(size):
+        
+        penup()
+        goto(-510+((1020/size)*columnCount),500)
+        pendown()
+        forward(1020)
+        columnCount+=1
 speed(0) 
+
+
+width()
+penup()
 grid()
-onkeypress(print("hello"),"a")
+
+
+
+
+
 listen()
 mainloop()
