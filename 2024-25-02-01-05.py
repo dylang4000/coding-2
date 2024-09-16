c1b2f712cf8a36b3e6ce6156013b64663e86694d
Dylan <davis948@student.pcsstn.net>
2024-09-16 09:45:28 -0500

    commit

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -1,48 +1,83 @@
-import tkinter
 from turtle import *
-import tkinter
+
+width(5)
 s=Screen()
-size=numinput('Enter an odd number',"Enter an odd number")
 s.setup(1020,1000)
+size=int(numinput('Enter an odd number',"Enter an odd number"))
+hideturtle()
 boxW=round(1020/size)
 boxH=round(1000/size)
 positionT=[]
 rowcount=0
+boxcount=0
+rowsCount=0
+columnCount=0
+keys=["1","2","3","4","5","6","7","8","9","r","o","y","g","b","i","v"]
 
-while size % 2 ==0:
+while size % 2 ==0 :
     size=numinput('Enter an ODD number',"Enter an ODD number")
 
-def nextrow():
-    goto(-507,500)
-    goto(-507,ycor()-boxH*rowcount)
 
-
-def square():
-    global size
-    global rowcount
-    global positionT
-    for i in range(int(size)):
-        positionT.append([xcor(),ycor()])
-        pendown()
-        forward(boxW)
-        right(90)
-        forward(boxH)
-        right(90)
-        forward(boxW)
-        right(90)
-        forward(boxH)
-        penup()
-        right(90)
-        forward(boxW)
-    rowcount+=1
 def grid():
+    global columnCount
+    global rowsCount
     penup()
-    goto(-507,500)    
+    goto(-510,500)
+    pendown()
+    rowsCount+=1
+    forward(1020)
+   
     for i in range(int(size)):
-        square()
-        nextrow()
+        
+        penup()
+        goto(-510,500-(boxW*rowsCount))
+        pendown()
+        forward(1020)
+        rowsCount+=1
+        
+    
+    right(90)
+    penup()
+    goto(-510,500)
+    pendown()
+    columnCount+=1
+    forward(1020)
+    for i in range(int(size)):
+        
+        penup()
+        goto(-510+((1020/size)*columnCount),500)
+        pendown()
+        forward(1000)
+        columnCount+=1
+        print(i)
 speed(0) 
+
+
+width(4)
+penup()
 grid()
-onkeypress(print("hello"),"a")
+
+def test():
+    print('asdasdf')
+
+onkeypress(test,"1")
+onkeypress(test,"2")
+onkeypress(test,"3")
+onkeypress(test,"4")
+onkeypress(test,"5")
+onkeypress(test,"6")
+onkeypress(test,"7")
+onkeypress(test,"8")
+onkeypress(test,"9")
+onkeypress(test,"0")
+onkeypress(test,"r")
+onkeypress(test,"o")
+onkeypress(test,"y")
+onkeypress(test,"g")
+onkeypress(test,"b")
+onkeypress(test,"i")
+onkeypress(test,"v")
+
+
 listen()
 mainloop()
