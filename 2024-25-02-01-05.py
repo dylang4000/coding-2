a8b995d8f994e5a84a3f822c7eff3a03d5280481
dylang4000 <davis948@student.pcsstn.net>
2024-09-17 09:45:39 -0500

    Update 2024-25-02-01-05.py

--- 2024-25-02-01-05.py
+++ 2024-25-02-01-05.py
@@ -2,9 +2,9 @@ from turtle import *
 
 width(5)
 s=Screen()
-s.setup(1020,1000)
+s.setup(1020,1050)
 size=int(numinput('Enter an odd number',"Enter an odd number"))
-hideturtle()
+#hideturtle()
 boxW=round(1020/size)
 boxH=round(1000/size)
 positionT=[]
@@ -12,6 +12,7 @@ rowcount=0
 boxcount=0
 rowsCount=0
 columnCount=0
+input=[]
 keys=["1","2","3","4","5","6","7","8","9","r","o","y","g","b","i","v"]
 
 while size % 2 ==0:
@@ -53,10 +54,64 @@ speed(0)
 width(4)
 penup()
 grid()
+penup()
+goto(-510,534)
+seth(0)
+pendown()
+begin_fill()
+color('dimgray')
+forward(1021)
+right(90)
+forward(30)
+right(90)
+forward(1021)
+right(90)
+forward(30)
+end_fill()
+penup()
+backward(30)
+right(90)
+forward(10)
+hideturtle()
+color('white')
+write('>>',font=('Arial',17, "normal"))
+forward(25)
+def r():
+    global input
+    write('r',font=('Arial',17, "normal"))
+    forward(10)
+    input.append('r')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+onkey(r,"r")
+onkey(r,"o")
+onkey(r,"y")
+onkey(r,"g")
+onkey(r,"b")
+onkey(r,"i")
+onkey(r,"v")
+onkey(r,"1")
+onkey(r,"2")
+onkey(r,"3")
+onkey(r,"4")
+onkey(r,"5")
+onkey(r,"6")
+onkey(r,"7")
+onkey(r,"8")
+onkey(r,"9")
+onkey(r,"0")
 
 listen()
 mainloop()
-
