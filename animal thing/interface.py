530e744af08ea372afe439ca9514482644441a48
Dylan <davis948@student.pcsstn.net>
2024-09-27 13:19:09 -0500

    commit

---
+++ animal thing/interface.py
@@ -0,0 +1,28 @@
+import turtle
+from rabbit import draw_rabbit
+from cow import draw_cow
+from giraffe import draw_giraffe
+from elephant import draw_elephant
+selector=0
+
+functionsanimals=[draw_rabbit,draw_cow,draw_giraffe,draw_elephant]
+turtle.speed(0)
+
+
+turtle.width(4)
+
+
+functionsanimals[selector](turtle)
+def select():
+    print('asdfasdf')
+    global selector
+    selector+=1
+    turtle.home()
+    turtle.clear()
+    functionsanimals[selector](turtle)
+    
+
+
+turtle.onkeypress(select,'a')
+turtle.listen()
+turtle.mainloop()
